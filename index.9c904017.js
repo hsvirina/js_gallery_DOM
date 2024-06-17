a3a4a2c8d398127c5b5062f6571d43b775c555f4
Hanna Svirina <26anna07@ukr.net>
2024-06-17 16:28:18 +0200

    make build

---
+++ index.9c904017.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var n=t.target.closest(".list-item__link");e.src=n.href});
+//# sourceMappingURL=index.9c904017.js.map
